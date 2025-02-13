469368445d20df0638a844da8d489662fb9af338
pmpknu <dangorlyakov@gmail.com>
2025-02-13 18:47:25 +0300

    feat(kernel): first kernel

--- run.sh
+++ run.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 set -xue
 
-# Путь QEMU 
 QEMU=qemu-system-riscv32
 
-# Запуск QEMU
-$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot
+# Путь к clang и его флагам
+CC=clang
+CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32 -ffreestanding -nostdlib"
 
+# Сборка ядра
+$CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
+    kernel.c
+
+# Запуск QEMU
+$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot \
+    -kernel kernel.elf
